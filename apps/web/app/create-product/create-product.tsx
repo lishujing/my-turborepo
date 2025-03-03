789003139f53416cab5d2a244378ccfb3514e7cb
lishujing <super_lishujing@163.com>
2025-03-03 22:50:47 +0800

    init: init project

---
+++ apps/web/app/create-product/create-product.tsx
@@ -0,0 +1,22 @@
+import createProduct from "./actions/create-product";
+
+export default async function CreateProduct() {
+    return (
+        <div>
+            <form action={createProduct}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+                <div>
+                    <label htmlFor="price">Price:</label>
+                    <input type="number" name="price" id="price" />
+                </div>
+                <button type="submit">
+                    提交
+                </button>
+            </form>
+        </div>
+
+    )
+}
